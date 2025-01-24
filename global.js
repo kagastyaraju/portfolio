e0178a68e6b10c534c114131be3c9b86d1f836dd
kagastyaraju <kagastyaraju@ucsd.edu>
2025-01-23 23:12:15 -0800

    Update global.js

--- global.js
+++ global.js
@@ -3,3 +3,14 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+// Step 2.1: Get all nav links
+let navLinks = $$("nav a");
+
+// Step 2.2: Find the link to the current page
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+// Step 2.3: Add the `current` class to the current page link
+currentLink?.classList.add("current");
